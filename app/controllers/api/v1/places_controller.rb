a31c0cab8d1566e259af88c72b386a1dde607dc1
luonglx <luonglx@runsystem.net>
2025-12-10 17:50:40 +0700

    feat: add PlacesController API

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,52 +1,43 @@
 module Api
   module V1
-    class PlacesController < ApplicationController
-      # include ApiAuthenticable
+    class PlacesController < Api::ApiController
+      skip_before_action :authenticate_user!
+
+      before_action -> { validate_query_fields!(Place) }, only: [:index]
+      before_action :set_place, only: [:show]
 
       def index
-        places = Place.all
-        places = places.where('city_en ILIKE ? OR city_vi ILIKE ?', "%#{params[:city]}%", "%#{params[:city]}%") if params[:city].present?
-        places = places.where('name_en ILIKE ? OR name_vi ILIKE ?', "%#{params[:query]}%", "%#{params[:query]}%") if params[:query].present?
+        places = Place.includes(place_sports: :sportfield)
+        places = places.ransack(params[:q]).result(distinct: true) if params[:q].present?
+        places = places.paginate(page: params[:page], per_page: params[:per_page])
 
-        render json: places.map { |p| place_json(p) }
+        render_success(serialize_resource(places), message: I18n.t('api.places.fetched_places_success'))
       end
 
       def show
-        place = Place.find(params[:id])
-        render json: place_json(place)
-      rescue ActiveRecord::RecordNotFound
-        render json: { error: 'Place not found' }, status: :not_found
+        render_success(serialize_resource(@place), message: I18n.t('api.places.fetched_place_success'))
       end
 
       private
 
-      def place_json(p)
-        {
-          place_id: p.place_id,
-          name_en: p.name_en,
-          name_vi: p.name_vi,
-          address_en: p.address_en,
-          address_vi: p.address_vi,
-          city_en: p.city_en,
-          city_vi: p.city_vi,
-          open_time: p.open_time,
-          close_time: p.close_time,
-          is_close: p.is_close,
-          maintenance_place: p.maintenance_place,
-          description_en: p.description_en,
-          description_vi: p.description_vi,
-          place_sports: p.place_sports.map do |ps|
-            {
-              id: ps.id,
-              sportfield_id: ps.sportfield_id,
-              sport_name: ps.sportfield&.name_en,
-              price_per_hour_usd: ps.price_per_hour_usd,
-              price_per_hour_vnd: ps.price_per_hour_vnd,
-              is_close: ps.is_close,
-              maintenance_sport: ps.maintenance_sport
-            }
-          end
-        }
+      def set_place
+        @place = Place.includes(place_sports: :sportfield).find(params[:id])
+      end
+
+      def serialize_resource(resource)
+        options = { include: included_associations }
+        
+        if resource.respond_to?(:each)
+          options[:each_serializer] = PlaceSerializer
+        else
+          options[:serializer] = PlaceSerializer
+        end
+
+        ActiveModelSerializers::SerializableResource.new(resource, options).as_json
+      end
+
+      def included_associations
+        ['place_sports', 'place_sports.sportfield']
       end
     end
   end
