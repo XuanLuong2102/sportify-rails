89c38e5f730b0eb5df5221080944edc103c0c13c
XuanLuong2102 <lexuanluongllhh@gmail.com>
2025-12-25 17:20:22 +0000

    feat: add active_storage for image, add default avatar for user

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -2,5 +2,9 @@ class ProductImage < ApplicationRecord
   belongs_to :product
   belongs_to :product_color, optional: true
 
+  has_one_attached :image do |attachable|
+    attachable.variant :image_50, resize_to_limit: [50, 50]
+    attachable.variant :image_300, resize_to_limit: [300, 300]
+  end
   validates :image_url, presence: true
 end
